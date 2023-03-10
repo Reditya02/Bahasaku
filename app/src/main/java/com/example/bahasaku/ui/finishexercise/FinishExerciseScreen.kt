6f0f80d5bbb0f137f9c85317338965788ac0a343
Reditya02 <putureditya@gmail.com>
2023-03-10 10:29:39 +0700

    Adding Navigation to Finish Exercise UI

--- app/src/main/java/com/example/bahasaku/ui/finishexercise/FinishExerciseScreen.kt
+++ app/src/main/java/com/example/bahasaku/ui/finishexercise/FinishExerciseScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bahasaku.core.theme.BahasakuTheme
+import com.ramcosta.composedestinations.annotation.Destination
 
+@Destination
 @Composable
 fun FinishExerciseScreen() {
     Surface(
