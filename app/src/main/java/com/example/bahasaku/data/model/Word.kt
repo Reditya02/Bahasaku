03b2c037e6d475cce253425ea78f635adc656a6c
Reditya02 <putureditya@gmail.com>
2023-04-28 12:05:16 +0800

    Adding Word Model

---
+++ app/src/main/java/com/example/bahasaku/data/model/Word.kt
@@ -0,0 +1,22 @@
+package com.example.bahasaku.data.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity
+@Parcelize
+data class Word(
+    @PrimaryKey
+    val id: String = "",
+    val chapterId: String = "",
+    val indonesian: String = "",
+    val balinese: String = "",
+    val imageUrl: String = "Hewan/anak_bebek.png",
+    val option: String = ""
+) : Parcelable {
+    fun getOptionList(): List<String> {
+        return option.split("$").shuffled()
+    }
+}
