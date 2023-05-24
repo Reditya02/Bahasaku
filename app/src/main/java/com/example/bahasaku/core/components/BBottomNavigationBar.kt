244f9252c0bbd938a13be330e187f341dfc7d0d5
Reditya02 <putureditya@gmail.com>
2023-05-25 00:20:23 +0700

    Update Navigation

--- app/src/main/java/com/example/bahasaku/core/components/BBottomNavigationBar.kt
+++ app/src/main/java/com/example/bahasaku/core/components/BBottomNavigationBar.kt
@@ -1,5 +1,9 @@
 package com.example.bahasaku.core.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -9,6 +13,9 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.bahasaku.R
 import com.example.bahasaku.core.navigation.BottomNavigationDestination
 import com.example.bahasaku.destinations.HomeScreenDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -24,18 +31,29 @@ fun BBottomNavigationBar(
         modifier = modifier,
         backgroundColor = MaterialTheme.colors.surface
     ) {
-        BottomNavigationDestination.values().forEach {
-            BottomNavigationItem(
-                selected = selected == it,
-                onClick = { navigator.navigate(it.direction) {
-                    popUpTo(HomeScreenDestination)
-                } },
-                icon = { Icon(imageVector = it.icon, contentDescription = "") },
-//                label = { Text(text = it.text) },
-                alwaysShowLabel = false,
-                selectedContentColor = MaterialTheme.colors.primary,
-                unselectedContentColor = MaterialTheme.colors.onBackground.copy(alpha = 0.6F)
+        Box(Modifier.fillMaxSize()) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(R.drawable.bg_bottom_nav_bar),
+                contentDescription = "",
+                contentScale = ContentScale.Crop
             )
+            Row {
+                BottomNavigationDestination.values().forEach {
+                    BottomNavigationItem(
+                        selected = selected == it,
+                        onClick = { navigator.navigate(it.direction) {
+                            popUpTo(HomeScreenDestination)
+                        } },
+                        icon = { Icon(imageVector = it.icon, contentDescription = "") },
+//                label = { Text(text = it.text) },
+                        alwaysShowLabel = false,
+                        selectedContentColor = MaterialTheme.colors.primary,
+                        unselectedContentColor = MaterialTheme.colors.onBackground.copy(alpha = 0.6F)
+                    )
+                }
+            }
         }
+
     }
 }
--- app/src/main/java/com/example/bahasaku/core/navigation/BottomNavigationDestination.kt
+++ app/src/main/java/com/example/bahasaku/core/navigation/BottomNavigationDestination.kt
@@ -3,10 +3,7 @@ package com.example.bahasaku.core.navigation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.example.bahasaku.destinations.DictionaryScreenDestination
-import com.example.bahasaku.destinations.HomeScreenDestination
-import com.example.bahasaku.destinations.LeaderboardScreenDestination
-import com.example.bahasaku.destinations.ProfileScreenDestination
+import com.example.bahasaku.destinations.*
 import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
 
 enum class BottomNavigationDestination(
@@ -15,6 +12,7 @@ enum class BottomNavigationDestination(
     val icon: ImageVector
 ) {
     HomeScreen(HomeScreenDestination, "Home", Icons.Default.Home),
+    ExerciseScreen(ListExerciseChapterScreenDestination, "Exercise", Icons.Default.CheckBox),
     DictionaryScreen(DictionaryScreenDestination, "Dictionary", Icons.Default.Book),
     LeaderboardScreen(LeaderboardScreenDestination, "Leaderboard", Icons.Default.Leaderboard),
     ProfileScreen(ProfileScreenDestination, "Profile", Icons.Default.Person)
