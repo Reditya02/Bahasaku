eba4034e26197c04ff913e9b4862428f85a3faf5
Reditya02 <putureditya@gmail.com>
2023-06-07 22:33:57 +0700

    implement SplashScreen.kt

--- app/src/main/java/com/example/bahasaku/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/bahasaku/ui/welcome/WelcomeScreen.kt
@@ -37,7 +37,6 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@RootNavGraph(start = true)
 @Destination
 @Composable
 fun WelcomeScreen(
